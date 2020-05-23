128e0a52e9960a53dcb0349e14a5a793495eddf9
theSunAndSnow <55275586+theSunAndSnow@users.noreply.github.com>
2020-05-24 04:42:35 +0800

    完成中间部分的轮播图以及轮播图下方的两张图片的布局

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -4,7 +4,7 @@
  * @Author: wangziyang
  * @Date: 2020-05-23 14:07:20
  * @LastEditors: wangziyang
- * @LastEditTime: 2020-05-23 21:25:17
+ * @LastEditTime: 2020-05-24 04:41:42
  */ 
 
 $(function() {
@@ -14,4 +14,34 @@ $(function() {
     $('.activities>li').mouseleave(function () { 
         $(this).find('p').prop('hidden', true);
     });
+
+    var imgs = document.querySelectorAll(".ImagesOfActivity img");
+    var loop = 0;
+    setInterval(function() {
+        loop += 1;
+        loop %= 2;
+        switchImg();
+
+    }, 2000);
+
+    $('#button0').on('click', function() {
+        loop = 0;
+        switchImg();
+    });
+
+    $('#button1').on('click', function() {
+        loop = 1;
+        switchImg();
+    });
+
+    function switchImg() {
+        for (var i = 0; i < imgs.length; ++i) {
+            imgs[i].style.display = 'none';
+        }
+
+        imgs[loop].style.display = 'block';
+
+        $('#button' + (loop)).addClass('currentButton');
+        $('#button' + (loop + 1) % 2).removeClass('currentButton');
+    }
 })
