0da85a18f75753e62e2a66efaf32a71c079a3c0c
theSunAndSnow <55275586+theSunAndSnow@users.noreply.github.com>
2020-05-25 02:00:21 +0800

    完成注册信息的验错功能

---
+++ target/KFCMavenWeb/js/index.js
@@ -0,0 +1,58 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: wangziyang
+ * @Date: 2020-05-23 14:07:20
+ * @LastEditors: wangziyang
+ * @LastEditTime: 2020-05-24 15:58:12
+ */ 
+
+$(function() {
+    // $('.activities>li').mouseenter(function () { 
+    //     $(this).find('p').prop('hidden', false);
+    // });
+    // $('.activities>li').mouseleave(function () { 
+    //     $(this).find('p').prop('hidden', true);
+    // });
+
+    $('.activities>li').mouseenter(function() {
+        $(this).find('p').slideDown(500);
+    });
+
+    $('.activities>li').mouseleave(function () { 
+        $(this).find('p').slideUp(500);
+    });
+
+    // 轮播图-start
+    var imgs = document.querySelectorAll(".ImagesOfActivity img");
+    var loop = 0;
+    setInterval(function() {
+        loop += 1;
+        loop %= 2;
+        switchImg();
+
+    }, 3000);
+
+    $('#button0').on('click', function() {
+        loop = 0;
+        switchImg();
+    });
+
+    $('#button1').on('click', function() {
+        loop = 1;
+        switchImg();
+    });
+
+    function switchImg() {
+        for (var i = 0; i < imgs.length; ++i) {
+            imgs[i].style.display = 'none';
+        }
+
+        imgs[loop].style.display = 'block';
+
+        $('#button' + (loop)).addClass('currentButton');
+        $('#button' + (loop + 1) % 2).removeClass('currentButton');
+    }
+
+    // 轮播图-end
+})
---
+++ target/KFCMavenWeb/js/order.js
@@ -0,0 +1,78 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: wangziyang
+ * @Date: 2020-05-20 09:17:19
+ * @LastEditors: wangziyang
+ * @LastEditTime: 2020-05-22 07:57:23
+ */ 
+$(function() {
+
+    var input = document.getElementsByTagName("input");
+
+    var total = 0;
+    var chickenWingNum = 0, chickenWingSetMealNum = 0,
+        beerNum = 0, hamburgerNum = 0, congeeNum = 0,
+        colaNum = 0;
+    
+    /**
+     * 获取用户点击事件，每次点击都更新总付款数
+     */ 
+    for (var i = 0; i < input.length; ++i) {
+        var j = i;
+
+        input[j].onclick = function() {
+            chickenWingNum = $("#chickenWing").val();
+            chickenWingSetMealNum = $("#chickenWingSetMeal").val();
+            beerNum = $("#beer").val();
+            hamburgerNum = $("#hamburger").val();
+            congeeNum = $("#congee").val();
+            colaNum = $("#cola").val();
+
+            total = chickenWingNum * 12.0 + chickenWingSetMealNum * 72.0 + 
+                beerNum * 15.0 + hamburgerNum * 20.0 + 
+                congeeNum * 10.0 + colaNum * 11.0;
+
+                $(".totalPayment").text("合计：" + total.toFixed(1)); // 精确到小数点后 1 位
+        };
+    }
+
+    /**
+     * 用户点击确认购买后，将购买数据发送给后端
+     */ 
+    $(".buyIt").click(function() {
+        $.get(
+            "buyServlet",
+            {
+                chickenWing : chickenWingNum,
+                chickenWingSetMeal : chickenWingSetMealNum,
+                beer : beerNum,
+                hamburger : hamburgerNum,
+                congee : congeeNum,
+                cola : colaNum,
+            },
+            function(data) {
+                alert("响应成功");
+                console.log(data);
+            },
+            "json"
+        ); 
+    }); 
+
+    var login = function() {
+        $.get(
+            "loginServlet",
+            {method : "status"},
+            function(data) {
+                console.log("登陆成功", data);
+                if (data[0].status == true) {
+                    console.log(data[1].telephone);
+                    $(".login").html("欢迎回来" + '<span>' + data[1].telephone + "</span>");
+                }
+            },
+            "json"
+        )
+    };
+
+    login(); // 每次页面刷新都会检查账号是否已经登陆
+});
